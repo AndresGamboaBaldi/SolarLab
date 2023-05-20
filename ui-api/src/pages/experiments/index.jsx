2b622a4dba294e1c6badb81cea081f826853e135
Andres Gamboa Baldi <andresgamboabaldi@gmail.com>
2023-05-19 23:30:37 -0400

    [SolarLab] UI - Update Logic of Booking Session now on Homepage

--- ui-api/src/pages/experiments/index.jsx
+++ ui-api/src/pages/experiments/index.jsx
@@ -6,50 +6,9 @@ import DepartamentExperiment from '../../components/DepartamentExperiment';
 import { Box, Typography, Grid } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
-import RingLoader from 'react-spinners/RingLoader';
 
 export default function Experiments() {
 	const [canAccess, setCanAccess] = useState(false);
-	const [loading, setLoading] = useState(true);
-	const [message, setMessage] = useState('Loading Session Info...');
-	const [initialState, setInitialState] = useState('first');
-	useEffect(() => {
-		reviewAccess();
-	}, [initialState]);
-	const reviewAccess = async () => {
-		const urlParams = new URLSearchParams(window.location.search);
-		const accessKey = urlParams.get('access_key');
-		if (accessKey != null) {
-			if (accessKey === 'admin') {
-				setCanAccess(true);
-			} else {
-				const pwd = urlParams.get('pwd');
-				const message = { access_key: accessKey, pwd: pwd };
-				const response = await fetch(`/api/booking`, {
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					method: 'POST',
-					body: JSON.stringify(message),
-				});
-				const data = await response.json();
-				if (data.status) {
-					setLoading(false);
-					setCanAccess(true);
-				} else {
-					setLoading(false);
-					setCanAccess(false);
-					setMessage('Oops... Your Session not started, come back later');
-				}
-			}
-		} else {
-			setLoading(false);
-			setMessage(
-				'Oops... Invalid Sesion, Click the link provided by Booking UPB'
-			);
-		}
-	};
-
 	return (
 		<main>
 			<div>
@@ -62,79 +21,56 @@ export default function Experiments() {
 					<link rel='icon' href='/logoYellow.png' />
 				</Head>
 			</div>
-			{canAccess ? (
-				<Box
-					mt={{ xxs: 10, xs: 10, s: 10, sm: 12 }}
-					px={{ xxs: 2, xs: 2, s: 4, sm: 6 }}
-				>
-					<Grid container>
-						<Grid
-							item
-							sx={{
-								display: 'flex',
-								alignItems: 'center',
-							}}
-							xxs={12}
-							xs={12}
-							sm={5}
-							md={3}
-							mb={{ xxs: 2, xs: 2, s: 2, sm: 0, md: 0 }}
-							justifyContent='flex-end'
-							order={{ xxs: 1, xs: 1, s: 1, sm: 2 }}
+			<Box
+				mt={{ xxs: 10, xs: 10, s: 10, sm: 12 }}
+				px={{ xxs: 2, xs: 2, s: 4, sm: 6 }}
+			>
+				<Grid container>
+					<Grid
+						item
+						sx={{
+							display: 'flex',
+							alignItems: 'center',
+						}}
+						xxs={12}
+						xs={12}
+						sm={5}
+						md={3}
+						mb={{ xxs: 2, xs: 2, s: 2, sm: 0, md: 0 }}
+						justifyContent='flex-end'
+						order={{ xxs: 1, xs: 1, s: 1, sm: 2 }}
+					>
+						<Typography variant='buttonsExperiments' color='primary.700'>
+							Remaining Time:
+						</Typography>
+						<Typography
+							ml={{ xxs: 1, xs: 1, s: 2, sm: 1 }}
+							variant='buttonsExperiments'
+							color='blacky.main'
 						>
-							<Typography variant='buttonsExperiments' color='primary.700'>
-								Remaining Time:
-							</Typography>
-							<Typography
-								ml={{ xxs: 1, xs: 1, s: 2, sm: 1 }}
-								variant='buttonsExperiments'
-								color='blacky.main'
-							>
-								10:32
-							</Typography>
-						</Grid>
-						<Grid
-							item
-							sx={{
-								display: 'flex',
-								alignItems: 'center',
-							}}
-							xxs={12}
-							xs={12}
-							sm={7}
-							md={9}
-							order={{ xxs: 2, xs: 2, s: 2, sm: 1 }}
-						>
-							<CitiesTypography></CitiesTypography>
-							<CitiesCheckboxes></CitiesCheckboxes>
-							<CitiesSelect></CitiesSelect>
-						</Grid>
+							10:32
+						</Typography>
 					</Grid>
-					<DepartamentExperiment></DepartamentExperiment>
-					<ExperimentButtons></ExperimentButtons>
-				</Box>
-			) : (
-				<Box
-					sx={{
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-						width: '100%',
-						height: '100vh',
-					}}
-				>
-					<Grid container rowSpacing={3}>
-						<Grid item xxs={12} align='center'>
-							<RingLoader size={96} color={'#F6BD2B'} loading={loading} />
-						</Grid>
-						<Grid item xxs={12} align='center'>
-							<Typography variant='buttonsExperiments' color='black'>
-								{message}
-							</Typography>
-						</Grid>
+					<Grid
+						item
+						sx={{
+							display: 'flex',
+							alignItems: 'center',
+						}}
+						xxs={12}
+						xs={12}
+						sm={7}
+						md={9}
+						order={{ xxs: 2, xs: 2, s: 2, sm: 1 }}
+					>
+						<CitiesTypography></CitiesTypography>
+						<CitiesCheckboxes></CitiesCheckboxes>
+						<CitiesSelect></CitiesSelect>
 					</Grid>
-				</Box>
-			)}
+				</Grid>
+				<DepartamentExperiment></DepartamentExperiment>
+				<ExperimentButtons></ExperimentButtons>
+			</Box>
 		</main>
 	);
 }
--- ui-api/src/pages/index.jsx
+++ ui-api/src/pages/index.jsx
@@ -3,7 +3,64 @@ import styles from '@/styles/Home.module.scss';
 import React, { useEffect, useState } from 'react';
 import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import HomeButtons from '../components/HomeButtons.jsx';
+import Alert from '../components/Alert.jsx';
 export default function Home() {
+	const [canAccess, setCanAccess] = useState(false);
+	const [showAlert, setShowAlert] = useState(false);
+	const [statusAlert, setStatusAlert] = useState('success');
+	const [messageAlert, setMessageAlert] = useState('');
+	const [initialState, setInitialState] = useState('first');
+
+	useEffect(() => {
+		reviewAccess();
+	}, [initialState]);
+
+	const reviewAccess = async () => {
+		const urlParams = new URLSearchParams(window.location.search);
+		const accessKey = urlParams.get('access_key');
+		if (accessKey != null) {
+			if (accessKey === 'admin') {
+				setCanAccess(true);
+			} else {
+				const pwd = urlParams.get('pwd');
+				const message = { access_key: accessKey, pwd: pwd };
+				const response = await fetch(`/api/booking`, {
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					method: 'POST',
+					body: JSON.stringify(message),
+				});
+				const data = await response.json();
+				if (data.status) {
+					setStatusAlert('info');
+					setMessageAlert(
+						'Your Session Has Started, Click the Button to Enter the UPB Remote Solar Lab'
+					);
+					setShowAlert(true);
+					setCanAccess(true);
+				} else {
+					setCanAccess(false);
+					setMessageAlert(
+						'Oops... Your Session has not Started, come back to the right time'
+					);
+					setShowAlert(true);
+					setStatusAlert('warning');
+				}
+			}
+		} else {
+			setMessageAlert(
+				'Oops... You dont have a Session, Book one and Click the Link to the Enter UPB Solar Remote Lab'
+			);
+			setShowAlert(true);
+			setStatusAlert('warning');
+		}
+	};
+
+	const handleCloseAlert = (event, reason) => {
+		setShowAlert(false);
+	};
+
 	return (
 		<main className={styles.main}>
 			<div className={styles.container}>
@@ -62,7 +119,12 @@ export default function Home() {
 					</div>
 				</CardContent>
 			</Card>
+			<Alert
+				open={showAlert}
+				handleClose={handleCloseAlert}
+				status={statusAlert}
+				message={messageAlert}
+			/>
 		</main>
 	);
 }
-
