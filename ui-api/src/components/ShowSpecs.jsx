3d776ceddeb3216eafd183f2d5a354f4cf820e0e
Andres Gamboa Baldi <andresgamboabaldi@gmail.com>
2023-06-25 19:40:07 -0400

    [SolarLab] UI - Add Images and Links to Specifications

--- ui-api/src/components/ShowSpecs.jsx
+++ ui-api/src/components/ShowSpecs.jsx
@@ -1,5 +1,14 @@
-import { Typography, Box, Grid, AppBar, Tabs, Tab } from '@mui/material';
+import {
+	Typography,
+	Box,
+	Grid,
+	AppBar,
+	Tabs,
+	Tab,
+	Button,
+} from '@mui/material';
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 import PropTypes from 'prop-types';
 function TabPanel(props) {
 	const { children, value, index } = props;
@@ -59,6 +68,12 @@ export default function ShowSpecs({ city }) {
 						allowScrollButtonsMobile
 					>
 						<Tab
+							label='Angle'
+							sx={{
+								textTransform: 'none',
+							}}
+						/>
+						<Tab
 							label='Solar Panel'
 							sx={{
 								textTransform: 'none',
@@ -77,25 +92,7 @@ export default function ShowSpecs({ city }) {
 							}}
 						/>
 					</Tabs>
-					<Grid
-						item
-						xxs={12}
-						xs={12}
-						sx={{
-							display: 'flex',
-							alignItems: 'center',
-						}}
-						justifyContent='center'
-					>
-						<Typography
-							variant='titleDepartment'
-							color='primary.700'
-							mt={2}
-							component={'span'}
-						>
-							Specifications
-						</Typography>
-					</Grid>
+
 					<Grid
 						item
 						xxs={12}
@@ -108,7 +105,40 @@ export default function ShowSpecs({ city }) {
 					>
 						<TabPanel value={value} index={0}>
 							<Grid container>
+								<Grid
+									item
+									xxs={12}
+									xs={12}
+									sx={{
+										display: 'flex',
+										alignItems: 'center',
+									}}
+									justifyContent='center'
+								>
+									<Typography
+										variant='titleDepartment'
+										color='primary.700'
+										mt={2}
+										component={'span'}
+									>
+										Information
+									</Typography>
+								</Grid>
 								<Grid item xxs={12} xs={12}>
+									<Typography
+										variant='titleDepartment'
+										color='blacky.main'
+										mr={1}
+										component={'span'}
+									>
+										Used Angle:
+									</Typography>
+								</Grid>
+							</Grid>
+						</TabPanel>
+						<TabPanel value={value} index={1}>
+							<Grid container>
+								<Grid item xxs={12} xs={12} mt={2}>
 									<Typography
 										variant='titleDepartment'
 										color='blacky.main'
@@ -121,6 +151,26 @@ export default function ShowSpecs({ city }) {
 										Photovoltaic Module I-75/12
 									</Typography>
 								</Grid>
+
+								<Grid
+									item
+									xxs={12}
+									xs={12}
+									sx={{
+										display: 'flex',
+										alignItems: 'center',
+									}}
+									justifyContent='center'
+								>
+									<Typography
+										variant='titleDepartment'
+										color='primary.700'
+										component={'span'}
+									>
+										Specifications
+									</Typography>
+								</Grid>
+
 								<Grid item xxs={12} xs={12}>
 									<Typography
 										variant='titleDepartment'
@@ -181,11 +231,34 @@ export default function ShowSpecs({ city }) {
 										17.3 V
 									</Typography>
 								</Grid>
+								<Grid
+									item
+									xxs={12}
+									xs={12}
+									mt={1}
+									sx={{
+										display: 'flex',
+									}}
+									justifyContent={'flex-end'}
+								>
+									<Button
+										variant='contained'
+										href='https://www.pvxchange.com/Solar-Modules/Isofoton/UL-I-75S-12_1-2100261'
+										sx={{
+											bgcolor: 'primary.700',
+											textTransform: 'none',
+										}}
+									>
+										<Typography variant='buttons2' color='white'>
+											More Info
+										</Typography>
+									</Button>
+								</Grid>
 							</Grid>
 						</TabPanel>
-						<TabPanel value={value} index={1}>
+						<TabPanel value={value} index={2}>
 							<Grid container>
-								<Grid item xxs={12} xs={12}>
+								<Grid item xxs={12} xs={12} mt={2}>
 									<Typography
 										variant='titleDepartment'
 										color='blacky.main'
@@ -197,6 +270,43 @@ export default function ShowSpecs({ city }) {
 										LP UVA 02
 									</Typography>
 								</Grid>
+								<Grid
+									item
+									xxs={12}
+									xs={12}
+									sx={{
+										display: 'flex',
+										alignItems: 'center',
+									}}
+									justifyContent='center'
+									my={1}
+								>
+									<Image
+										src='/lpUVA.jpg'
+										alt='Upb Logo White'
+										width={144}
+										height={144}
+									/>
+								</Grid>
+								<Grid
+									item
+									xxs={12}
+									xs={12}
+									sx={{
+										display: 'flex',
+										alignItems: 'center',
+									}}
+									justifyContent='center'
+								>
+									<Typography
+										variant='titleDepartment'
+										color='primary.700'
+										component={'span'}
+									>
+										Specifications
+									</Typography>
+								</Grid>
+
 								<Grid item xxs={12} xs={12}>
 									<Typography
 										variant='titleDepartment'
@@ -209,11 +319,34 @@ export default function ShowSpecs({ city }) {
 										38.5 u(W/m2)
 									</Typography>
 								</Grid>
+								<Grid
+									item
+									xxs={12}
+									xs={12}
+									mt={1}
+									sx={{
+										display: 'flex',
+									}}
+									justifyContent={'flex-end'}
+								>
+									<Button
+										variant='contained'
+										href='https://www.deltaohm.com/wp-content/uploads/document/DeltaOHM-LPUVA02-manual-en.pdf'
+										sx={{
+											bgcolor: 'primary.700',
+											textTransform: 'none',
+										}}
+									>
+										<Typography variant='buttons2' color='white'>
+											More Info
+										</Typography>
+									</Button>
+								</Grid>
 							</Grid>
 						</TabPanel>
-						<TabPanel value={value} index={2}>
+						<TabPanel value={value} index={3}>
 							<Grid container>
-								<Grid item xxs={12} xs={12}>
+								<Grid item xxs={12} xs={12} mt={2}>
 									<Typography
 										variant='titleDepartment'
 										color='blacky.main'
@@ -225,6 +358,44 @@ export default function ShowSpecs({ city }) {
 										Pyranometer CMP6
 									</Typography>
 								</Grid>
+								<Grid
+									item
+									xxs={12}
+									xs={12}
+									sx={{
+										display: 'flex',
+										alignItems: 'center',
+									}}
+									justifyContent='center'
+									my={1}
+								>
+									<Image
+										src='/CMP6.jpg'
+										alt='Upb Logo White'
+										width={144}
+										height={144}
+									/>
+								</Grid>
+								<Grid
+									item
+									xxs={12}
+									xs={12}
+									sx={{
+										display: 'flex',
+										alignItems: 'center',
+									}}
+									justifyContent='center'
+								>
+									<Typography
+										variant='titleDepartment'
+										color='primary.700'
+										mt={2}
+										component={'span'}
+									>
+										Specifications
+									</Typography>
+								</Grid>
+
 								<Grid item xxs={12} xs={12}>
 									<Typography
 										variant='titleDepartment'
@@ -236,6 +407,29 @@ export default function ShowSpecs({ city }) {
 									<Typography variant='dataDepartment' color='blacky.main'>
 										15.87 u(W/m2)
 									</Typography>
+								</Grid>
+								<Grid
+									item
+									xxs={12}
+									xs={12}
+									mt={1}
+									sx={{
+										display: 'flex',
+									}}
+									justifyContent={'flex-end'}
+								>
+									<Button
+										variant='contained'
+										href='https://www.kippzonen.com/Product/12/CMP6-Pyranometer#.Ymw4ztrMJPZ'
+										sx={{
+											bgcolor: 'primary.700',
+											textTransform: 'none',
+										}}
+									>
+										<Typography variant='buttons2' color='white'>
+											More Info
+										</Typography>
+									</Button>
 								</Grid>
 							</Grid>
 						</TabPanel>
