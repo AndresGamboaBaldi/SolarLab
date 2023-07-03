d60cd0983b63d8c42152c8ba695c8b941801cb31
Andres Gamboa Baldi <andresgamboabaldi@gmail.com>
2023-07-02 23:00:24 -0400

    [SolarLab] DB - Implement DataWatcher Server to Save Received Data on DB

---
+++ DataWatcher/app/app.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+
+var mysql = require("mysql2");
+
+var connection = mysql.createConnection({
+  host: "research.upb.edu",
+  port: "63307",
+  user: "root",
+  password: "root",
+  database: "solar_lab_db",
+  flags: ["+LOCAL_FILES"],
+});
+
+const query =
+  `LOAD DATA LOCAL INFILE ? INTO TABLE Datalogger ` +
+  `FIELDS TERMINATED BY ? ` +
+  `ENCLOSED BY ? ` +
+  `LINES TERMINATED BY ? ` +
+  `IGNORE ? ROWS ` +
+  `(@datetime,record,solarRadiationCMP,solarRadiationCMPAvg, uvaRadiationLP, uvaRadiationLPAvg, batteryVoltage, dataloggerTemperature, voltage, current, solarRadiationCS320) ` +
+  `SET datetime = STR_TO_DATE(@datetime,'%Y-%m-%d %H:%i:%s')`;
+
+const path = "./Data/Radiacion_Min5.dat";
+
+connection.connect(function (err) {
+  if (err) throw err;
+});
+
+fs.watchFile(
+  path,
+  {
+    // Passing the options parameter
+    bigint: false,
+    persistent: true,
+    interval: 1000,
+  },
+  (curr, prev) => {
+    connection.query(
+      "SELECT COUNT(*) FROM Datalogger",
+      function (err, result, fields) {
+        if (err) throw err;
+        if (result[0]["COUNT(*)"] <= 0) {
+          connection.query(
+            {
+              sql: query,
+              values: [path, ",", '"', "\n", 4],
+              infileStreamFactory: () => fs.createReadStream(path),
+            },
+            (err, _ok) => {
+              if (err) {
+                console.log(err);
+                throw err;
+              }
+              ok = _ok;
+            }
+          );
+        } else {
+          var ignoredRows = 4 + result[0]["COUNT(*)"];
+          connection.query(
+            {
+              sql: query,
+              values: [path, ",", '"', "\n", ignoredRows],
+              infileStreamFactory: () => fs.createReadStream(path),
+            },
+            (err, _ok) => {
+              if (err) {
+                console.log(err);
+                throw err;
+              }
+              ok = _ok;
+            }
+          );
+        }
+      }
+    );
+  }
+);
